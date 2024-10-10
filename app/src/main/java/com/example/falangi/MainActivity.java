8a57773e03ec898d35089a188ccb8b6e22addb2c
Mardragox <krzysieksot@gmail.com>
2024-10-10 11:43:57 +0200

    ginger

--- app/src/main/java/com/example/falangi/MainActivity.java
+++ app/src/main/java/com/example/falangi/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.falangi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private int Counter = 0;
+
+    private TextView textViewPolecenie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewPolecenie = findViewById(R.id.textViewPolecenie);
+    }
+
+    public void WrongButton(View view) {
+        Toast.makeText(this, "Ten kolor jest we fladze Polski", Toast.LENGTH_SHORT).show();
+    }
+
+    public void GoodButton(View view) {
+        Toast.makeText(this, "Dobrze żeś kliknął", Toast.LENGTH_SHORT).show();
+        view.setVisibility(View.INVISIBLE);
+        Counter++;
+        if (Counter == 4){
+            textViewPolecenie.setText("Brawooo");
+        }
     }
 }
